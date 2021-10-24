ef8deae2778671744c908156aaf02c73b3040fed
jh <bndgc@naver.com>
2021-10-24 16:40:36 +0900

    [Add] PostingView - post

--- posting/views.py
+++ posting/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from posting.models import Posting
+from core.views import login_required
+
+class PostingView(View):
+    @login_required
+    def post(self, request):
+        try:
+            data    = json.loads(request.body)
+            title   = data['title']        
+            content = data['content']        
+            user    = request.user
+
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
+
+        Posting.objects.create(
+            title   = title,
+            content = content,
+            user_id = user.id,
+        )
+
+        return JsonResponse({"message" : "CREATED"}, status = 201)
+    
+    @login_required
+    def get(self, request):
+        postings = Posting.objects.all()
+
+        result = [{
+            "id" : posting.id,
+            "title" : posting.title,
+        } for posting in postings]
+
+        return JsonResponse({"result" : result}, status = 200)
