a1bef573fa12b5d38b2a5cc75be4756049d8aa4f
jh <bndgc@naver.com>
2021-10-24 14:51:49 +0900

    [Add] db modeling

--- posting/models.py
+++ posting/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from core.models import TimeStampModel 
 
-# Create your models here.
+class Posting(TimeStampModel):
+    title = models.CharField(max_length = 100, unique = True)
+    content = models.TextField()
+    user = models.ForeignKey('user.User',on_delete = models.CASCADE)
+    
+    class Meta:
+        db_table = 'postings'
+
+    def __str__(self):
+        return self.title
--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from core.models import TimeStampModel 
 
-# Create your models here.
+class User(TimeStampModel):
+    email = models.CharField(max_length = 100, unique = True)
+    nickname = models.CharField(max_length = 20, unique = True)
+    password = models.CharField(max_length = 100)
+    
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return self.nickname
